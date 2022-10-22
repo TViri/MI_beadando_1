5e31366912893d3b56147e9bb967b31e45fabb62
TViri <virag.tibenszki@stud.uni-corvinus.hu>
2022-10-22 19:52:25 +0200

    Commit

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,5 +1,23 @@
 import pandas as pd
-header_list = ['Ip-cím']
-df = pd.read_csv('ip.txt', names = header_list)
+header_list = ['IP-cím']
+df = pd.read_csv('ip.txt', names=header_list)
+print("Adatsorok az állományban: ", len(df.index) )
 
-print('Adatsorok az állományban: ', len(df.index))
+documentAddressesCount = 0      
+itAccessoriesCount = 0          
+localAddresssesCount = 0        
+
+for index, row in df.iterrows():
+    actualRow = (row['IP-cím'])
+    if (actualRow.startswith('fc') or actualRow.startswith('fd')):
+        localAddresssesCount += 1
+    else:
+        if actualRow.startswith('2001:0e'):
+            itAccessoriesCount += 1
+        else:
+            if actualRow.startswith('2001:0db8'):
+                documentAddressesCount += 1
+
+print("Dokumentációs címek száma: ", documentAddressesCount )
+print("IT eszközöknek kiosztott globális címek száma: ", itAccessoriesCount )
+print("Eszközöknek kiosztott helyi egyedi címek száma: ", localAddresssesCount )
